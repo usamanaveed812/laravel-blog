bef50480ec97bd5e938866fc9b158cf04636e86b
root <root@ubuntu.5e41le2bajuexlwutcyutyrdvh.zx.internal.cloudapp.net>
2022-09-08 12:33:20 +0000

    changes

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+ Route::get('/', function () {
+     return view('welcome');
+ });
 
 
 Route::get('/post',[PostController::class,'index']);
